f10f5df28e7e5990085e0c03e41e48cdde947f96
Mo Khan <mo.khan101010@yahoo.com>
2024-12-19 14:39:10 +0000

    master: Added a new Customer field called 'Address'.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -6,6 +6,8 @@
         public string? Name { get; set; }
         public string? Email { get; set; }
 
+        public string? Address { get; set; } // New property
+
         public Customer()
         {
             
